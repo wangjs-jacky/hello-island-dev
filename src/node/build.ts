628de4bacdced13591aedf0f5928875116e398c5
所无聊 <2409277719@qq.com>
2023-02-04 16:29:08 +0800

    打包两份代码,使用 ssr-entry 的 render 函数去生成 html 页面, 使用 client-entry 去注水操作

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,11 +1,15 @@
 import { build as viteBuild, InlineConfig } from "vite";
 import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from "./constants";
+import type { RollupOutput } from "rollup";
+import { join } from "path";
+import * as fs from "fs-extra";
 
 export async function bundle(root: string) {
   const resolveViteConfig = (isServer: boolean = false): InlineConfig => ({
     mode: "production",
     root,
     build: {
+      ssr: isServer,
       outDir: isServer ? ".temp" : "build",
       rollupOptions: {
         input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
@@ -17,24 +21,56 @@ export async function bundle(root: string) {
   })
 
   try {
-    const clientBuild = async () => {
-      return viteBuild(resolveViteConfig())
-    }
-    const serverBuild = async () => {
-      return viteBuild(resolveViteConfig(true));
-    }
     console.log("Building client + server bundles...");
-    Promise.all([clientBuild(), serverBuild()]);
+    const [clientBundle, serverBundle] = await Promise.all([
+      /* client build */
+      viteBuild(resolveViteConfig()),
+      /* server build */
+      viteBuild(resolveViteConfig(true))
+    ]);
+    return [clientBundle, serverBundle] as [RollupOutput, RollupOutput]
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function build(root: string) {
-  /* 1. bundle - client 端 + server 端 */
-  console.log("root", root);
-  await bundle(root);
-  /* 2. 引入 server-entry 模块 */
+export async function renderPage(
+  render: () => string,
+  root: string,
+  clientBundle: RollupOutput
+) {
+  const appHtml = render(); // 将 renderToString 产出的 HTML 嵌入模板
 
-  /* 3. 服务端渲染 */
+  /* hybrite 注入客户端的 js 脚本 */
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === "chunk" && chunk.isEntry
+  );
+
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <title>title</title>
+      <meta name="description" content="xxx">
+    </head>
+    <body>
+      <div id="root">${appHtml}</div>
+      <script type="module" src="/${clientChunk?.fileName}"></script>
+    </body>
+  </html>`.trim();
+  await fs.writeFile(join(root, 'build', 'index.html'), html);
+  /* 删除临时文件 .temp（仅用于使用 renderToString 获取 html 文本） */
+  await fs.remove(join(root, '.temp'));
+}
+
+export async function build(root: string = ".") {
+  /* 1. bundle - client 端 + server 端 */
+  const [clientBundle, serverBundle] = await bundle(root);
+  /* 2. 使用编译后的 server-entry 模块导出的 render 函数 */
+  const serverEntryPath = join(process.cwd(), root, ".temp", "ssr-entry.js");
+  const { render } = require(serverEntryPath); /* 使用 require 导入 CJS 包*/
+  /* 3. 服务端渲染, 产出 HTML */
+  renderPage(render, root, clientBundle);
 }
