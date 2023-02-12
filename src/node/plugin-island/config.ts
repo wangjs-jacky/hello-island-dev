a9eb17f2fae743c9404c125a19dbf42ef7d7dcbc
所无聊 <2409277719@qq.com>
2023-02-12 10:49:48 +0800

    💥 feat: config.ts 文件热更新第一种方案, 失败.

--- src/node/plugin-island/config.ts
+++ src/node/plugin-island/config.ts
@@ -1,5 +1,6 @@
 import { SiteConfig } from "../../shared/types";
-import { Plugin } from "vite";
+import { Plugin, ViteDevServer } from "vite";
+import { relative } from "node:path";
 
 /* 虚拟模块:
   作用等价于 umi 中的运行时概念，因为在编译阶段通过 fs.readFile 是轻松获取到 config 配置的。
@@ -8,6 +9,7 @@ import { Plugin } from "vite";
 const SITE_DATA_ID = "island:site-data";
 
 export function pluginConfig(config: SiteConfig): Plugin {
+  let server: ViteDevServer = null;
   return {
     name: "island:config",
     resolveId(id) {
@@ -19,6 +21,28 @@ export function pluginConfig(config: SiteConfig): Plugin {
     load(id) {
       if (id === "\0" + SITE_DATA_ID) {
         return `export default ${JSON.stringify(config.siteData)}`;
+      }
+    },
+    /* 通过 configureServer 可以获取 server 实例 */
+    configureServer(s) {
+      server = s;
+    },
+    async handleHotUpdate(ctx) {
+      /* 监听范围 */
+      const customWatchedFiles = [config.configPath];
+      /* 筛选是否为 config.ts 文件 */
+      const isConfigFile = (id: string) => {
+        return customWatchedFiles.some((file) => id.includes(file));
+      };
+
+      if (isConfigFile(ctx.file)) {
+        console.log(
+          `\n${relative(
+            config.root,
+            ctx.file
+          )} changed, restarting server ......`
+        );
+        await server.restart();
       }
     },
   };
